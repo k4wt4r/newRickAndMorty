7b159824861552e12dad73b2b775040ce6ae48ca
k4wt4r <kawtarelalmi0@gmail.com>
2022-06-16 18:33:27 +0100

    Feat[characters]: Added pagination

--- my-app/src/components/compositions/pages/CharactersPage/CharactersPage.style.tsx
+++ my-app/src/components/compositions/pages/CharactersPage/CharactersPage.style.tsx
@@ -2,10 +2,12 @@ import styled from "styled-components";
 
 export const StyledCharactersContainer = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   width: 100%;
   height: 100%;
+  padding: 40px;
 `;
 
 export const StyledContainer = styled.div`
@@ -13,11 +15,37 @@ export const StyledContainer = styled.div`
   height: 100%;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
-  gap: 10px;
+  gap: 8px;
 `;
 
 export const StyledCardCharacters = styled.div`
   display: flex;
   padding: 6px;
   border-radius: 30px;
+`;
+
+export const StyledPagination = styled.div`
+  width: 200px;
+  height: 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const StyledButtonNext = styled.button`
+  background-color: #1b1d24;
+  color: #fffefe;
+  border: 1px solid #c9c9c9;
+  width: 90px;
+  height: 30px;
+  font-size: 16px;
+  font-weight: 300;
+`;
+export const StylesButtonPrevious = styled.button`
+  background-color: #1b1d24;
+  color: #fffefe;
+  border: 1px solid #c9c9c9;
+  width: 90px;
+  height: 30px;
+  font-size: 16px;
+  font-weight: 300;
 `;
--- my-app/src/components/compositions/pages/CharactersPage/CharactersPage.tsx
+++ my-app/src/components/compositions/pages/CharactersPage/CharactersPage.tsx
@@ -2,30 +2,43 @@ import { ICharacter } from "@/src/interfaces";
 import React, { useEffect, useState } from "react";
 import CardCharacters from "../../Card/CardCharacters";
 import * as S from "./CharactersPage.style";
+import { IoIosArrowBack } from "react-icons/io";
 
 const CharactersPage = () => {
   const [characters, setCharacters] = useState<ICharacter[]>([]);
+  const [page, setPage] = useState(1);
+  let api = `https://rickandmortyapi.com/api/character/?page=${page}`;
 
   useEffect(() => {
     (async () => {
-      const response = await fetch(
-        "https://rickandmortyapi.com/api/character/"
-      );
+      const response = await fetch(api);
       const data = await response.json();
       setCharacters(data.results);
     })();
-  }, []);
+  }, [api]);
 
   return (
-    <S.StyledCharactersContainer>
-      <S.StyledContainer>
-        {characters.map((character, id) => (
-          <S.StyledCardCharacters key={id}>
-            <CardCharacters character={character} />
-          </S.StyledCardCharacters>
-        ))}
-      </S.StyledContainer>
-    </S.StyledCharactersContainer>
+    <>
+      <S.StyledCharactersContainer>
+        <S.StyledContainer>
+          {characters.map((character, id) => (
+            <S.StyledCardCharacters key={id}>
+              <CardCharacters character={character} />
+            </S.StyledCardCharacters>
+          ))}
+        </S.StyledContainer>
+        <S.StyledPagination>
+          <S.StylesButtonPrevious onClick={() => setPage(page - 1)}>
+            <IoIosArrowBack />
+            PREV
+          </S.StylesButtonPrevious>
+          <div>{page}</div>
+          <S.StyledButtonNext onClick={() => setPage(page + 1)}>
+            NEXT
+          </S.StyledButtonNext>
+        </S.StyledPagination>
+      </S.StyledCharactersContainer>
+    </>
   );
 };
 
